db08a5d9626155d19c5e4b8326df63036e37135b
Brandon Vernon <brandon.vernon@materialplus.io>
2023-09-26 15:45:14 -0500

    small updates and bug fixes

--- js/main.js
+++ js/main.js
@@ -1,43 +1,4 @@
-/*** Variables ***/
-
-// Tabindex Variables
-//const zero = document.getElementsByClassName('zero');
-
-// Variables: Nav
-const checkbox = document.getElementById('checkbox'),
-  navMobile = document.getElementById('navMobile')
-
-// Variables: Header
-const header = document.getElementById('header'),
-  headerLoadBG = document.getElementById('headerLoadBG'),
-  headerLoadBGName = document.getElementById('headerLoadBGName'),
-  firstHeadline = document.getElementsByTagName('h1')[0]
-
-/*
-
-// Variables: Functions
-let timeout = false, // holder for timeout id
-    delay = 250; // delay after event is "complete" to run callback
-
-*/
-
-/*** Event Listeners ***/
 window.addEventListener('load', windowLoad, false)
-window.addEventListener('click', windowClick, false)
-
-/*
-
-window.addEventListener('keydown', keydown, false);
-window.addEventListener('resize', function() {
-  // clear the timeout
-  clearTimeout(timeout);
-  // start timing for event "completion"
-  timeout = setTimeout(windowResize, delay);
-});
-
-*/
-
-/*** Event Listeners' Functions ***/
 
 // Apply Correct Styles on Window Load
 var ua = window.navigator.userAgent
@@ -45,12 +6,18 @@ var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i)
 var webkit = !!ua.match(/WebKit/i)
 var iOSSafari = iOS && webkit && !ua.match(/CriOS/i)
 
+const header = document.getElementById('header'),
+  headerLoadBG = document.getElementById('headerLoadBG'),
+  headerLoadBGName = document.getElementById('headerLoadBGName'),
+  firstHeadline = document.getElementsByTagName('h1')[0]
+
 function windowLoad() {
   if (!iOSSafari) {
     if (window.location.href == 'https://www.brandonvernon.com/') {
       headerLoadBGName.style.opacity = '1'
-    } else { // Remove for Prod
-      headerLoadBGName.style.opacity = '1'
+    } else {
+      headerLoadBGName.style.opacity = '1' // Remove for Prod
+      firstHeadline.style.opacity = '1'
     }
     setTimeout(function () {
       headerLoadBG.style.height = '0px'
@@ -67,66 +34,23 @@ function windowLoad() {
   }
 }
 
-// Call Specific Functions on Click
-function windowClick(e) {
-  if (e.target === checkbox) {
-    toggleMobileNav();
-  } else {
-    return
-  }
-}
+// Footer: Get current year
+const year = document.querySelector('.year')
 
-/*
-
-// Call Specific Functions on Keydown
-function keydown(e) {
-  if (e.keyCode === 13) {
-
-  } else if (e.keyCode === 27) {
-
-  };
-};
-
-function windowResize() {
-
-};
-
-*/
-
-/*** Common Functions ***/
-function toggleMobileNav() {
-  if (checkbox.checked === true) {
-    navMobile.style.height = '100VH'
-  } else if (checkbox.checked === false) {
-    setTimeout(function () {
-      navMobile.style.height = '72px'
-    }, 500)
-  }
-}
-
-/*
-
-// Tabindex
-function tabindex(array, int) {
-  for (let i = 0; i < array.length; i++) {
-    array[i].tabIndex = int;
-  };
-};
+year.innerHTML = new Date().getFullYear()
 
 // Disable Scroll
 function disableScroll() {
   // Get the current page scroll position
-  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
   // if any scroll is attempted, set this to the previous value
   window.onscroll = function() {
-    window.scrollTo(scrollLeft, scrollTop);
-  };
-};
+    window.scrollTo(scrollLeft, scrollTop)
+  }
+}
 
 // Enable Scroll
 function enableScroll() {
-  window.onscroll = function() {};
-};
-
-*/
+  window.onscroll = function() {}
+}
